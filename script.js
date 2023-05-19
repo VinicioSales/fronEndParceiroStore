721cb9062b743a296899a922a8057e1a1175064d
VinicioSales <vinicio.santtana@gmail.com>
2023-05-19 00:34:56 -0300

    Demo final

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
+const buttons = document.querySelectorAll('.botoes');
+const modal = document.querySelector('#myModal');
+const modalClose = document.querySelector('.close');
 const button = document.querySelector('#openModalBtn')
-const modal = document.querySelector('#myModal')
-const modalClose = document.querySelector('.close')
+const modalForms = document.getElementById('modalForms');
+const openModalFormsButton = document.getElementById('openModalFormsBtn');
+const closeModalFormsButton = document.getElementById('closeModalFormsBtn');
 
-
-
-// Função para abrir o modal
+// Function to open the modal
 function openModal() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
-// Função para fechar o modal
+// Function to close the modal
 function closeModal() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
-
-
-button.addEventListener('click', event => {
-    modal.style.display = "block";
+// Event listener for each "Comprar" button
+buttons.forEach(button => {
+  button.addEventListener('click', openModal);
 });
 
+// Event listener to close the modal when clicking outside of it or on the close button
 document.addEventListener('click', event => {
-    if (!modal.contains(event.target) && event.target !== button) {
-      modal.style.display = 'none';
-    }
-  });
+if (!modal.contains(event.target) && event.target !== button && !buttons.includes(event.target)) {
+    modal.style.display = 'none';
+}
+});
 
-/*modalClose.addEventListener('click', () => {
-    modal.style.display = "none";
-})*/
+openModalFormsButton.addEventListener('click', () => {
+  modalForms.style.display = 'block';
+});
+
+closeModalFormsButton.addEventListener('click', () => {
+  modalForms.style.display = 'none';
+});
+
+
+// Close the modal when clicking on the close button
+modalClose.addEventListener('click', closeModal);
